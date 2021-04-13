341e960b0dc806d984c30b54dcd72eb9fc665ff0
Kyrollos <kirobekhet@gmail.com>
2021-04-13 18:44:22 -0400

    adding the DLL and the Windows Form application

---
+++ DLL_CS_PYTHON_SETUP/DLL_CS_PYTHON_SETUP/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculate
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Substract(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public int Devide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
